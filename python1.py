4308d3e5679640c51c2ef4c41e6a3499c1b34b36
juadacaf <128726718+juadacaf@users.noreply.github.com>
2023-10-31 11:41:19 -0500

    2

--- python1.py
+++ python1.py
@@ -5,24 +5,62 @@ Created on Tue Oct 31 11:39:19 2023
 @author: 57321
 """
 
-def peones_se_amanazan(peon1, peon2):
-    x1, y1 = peon1
-    x2, y2 = peon2
 
-    
-    if abs(x1 - x2) == 1 and abs(y1 - y2) == 1:
-        return True
+
+
+
+    def torres(torre1, torre2):
+        x1, y1 = torre1
+        x2, y2 = torre2
+
+       
+        if x1 == x2 or y1 == y2:
+            return True
+        else:
+            return False
+
+
+    torre1 = (2, 3)  
+    torre2 = (4, 3)  
+
+    if torres(torre1, torre2):
+        print("Las torres se amenazan mutuamente.")
     else:
-        return False
+        print("La defecaste no sirves ni para matar una torre")
+        
+    def alfiles(alfil1, alfil2):
+        x1, y1 = alfil1
+        x2, y2 = alfil2
 
+      
+        if abs(x1 - x2) == abs(y1 - y2):
+            return True
+        else:
+            return False
 
-peon1 = (4, 3)  
-peon2 = (5, 3)  
+    # Ejemplo de uso
+    alfil1 = (2, 2) 
+    alfil2 = (7, 7)  
+    if alfiles(alfil1, alfil2):
+        print("Los alfiles se amenazan mutuamente.")
+    else:
+        print("La defecaste no sirves ni para matar un alfil")
+        
+    def peones_se_amanazan(peon1, peon2):
+        x1, y1 = peon1
+        x2, y2 = peon2
 
-if peones_se_amanazan(peon1, peon2):
-    print("Los peones se amenazan mutuamente.")
-else:
-    print("La defecaste no sirves ni para amenazar un peón:(")
+        # Verificar si los peones están uno casillero diagonalmente el uno del otro
+        if abs(x1 - x2) == 1 and abs(y1 - y2) == 1:
+            return True
+        else:
+            return False
 
+    # Ejemplo de uso
+    peon1 = (4, 1)  # Coordenadas del primer peón (fila, columna)
+    peon2 = (5, 2)  # Coordenadas del segundo peón (fila, columna)
 
-
+    if peones_se_amanazan(peon1, peon2):
+        print("Los peones se amenazan mutuamente.")
+    else:
+        print("La defecaste no sirves ni para amenazar un peón")
